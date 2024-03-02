502f7102fc2e59949723b58b88f9c84cb946c5ce
luisftexeira <luisftexeira@gmail.com>
2024-03-01 22:14:35 -0300

    responsive-update

--- js/nav-efeito.js
+++ js/nav-efeito.js
@@ -12,3 +12,26 @@ window.addEventListener('scroll', function () {
 
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navList = document.querySelector(".nav-list");
+    const navLinks = document.querySelectorAll(".nav-list .nav__link");
+
+    menuToggle.addEventListener("click", function() {
+        navList.classList.toggle("active");
+    });
+
+    // Adiciona um evento de clique a cada link do menu
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener("click", function() {
+            // Fecha a barra de navegação removendo a classe 'active'
+            navList.classList.remove("active");
+        });
+    });
+});
+
+
+
+
